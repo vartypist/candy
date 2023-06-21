f174af76ef9f3153639b5dc855905ac54cff109f
vartypist <130790582+vartypist@users.noreply.github.com>
2023-06-21 19:20:38 +0200

    Create Candycrush.cpp

---
+++ Candycrush.cpp
@@ -0,0 +1,12 @@
+// candycrush.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Queue.h"
+// #include "matrice.h"
+
+int main() {
+    etQueue stQueueAction;
+    InitialiserQueue(&stQueueAction);
+    return 0;
+}
